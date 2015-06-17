05c89c0f45319c09f0a28ba45a9dbc6bfa269d61
SDMitchell <Shawn.Mitchell@unb.ca>
2015-06-16 23:25:32 -0300

    Finished assignment and unit test

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## The gist of this code is to provide a class-like wrapper for a basic R matrix via scoping rules and a dispatch list.
+##
 
-## Write a short comment describing this function
+## makeCacheMatrix - This is a factory function that creates a class-like wrapper around the passed-in argument x;
+##    x is cached and the inverse of x (represented by m) is NULLed. Four operations (get(), set(), getInverse(),
+##    setInverse()) are declared and have access to both x and its inverse via the scoping operator <<-. A list
+##    containing each of the functions is returned so that they may be called by the owner of the wrapper.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(inv) m <<- inv
+	getInverse <- function() m
+	list(set = set,
+		get = get,
+		setInverse = setInverse,
+		getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve - This method either computes the inverse of a wrapped matrix x (if it is the first time being computed) or
+##    simply returns a previously cached result. If the result is being computed for the first time, it is stored via
+##    a call to the setInverse() method so that any further calls will return the cached inverse rather than recomputing
+##    the inverse from scratch. An intervening call to set() will NULL the cached inverse and will cause a recomputation to take place.
+##    The call to set() is not demonstrated here.
+##
+##    The internal call to solve() may be affected by the caller via the extra arguments taken by the cacheSolve() function;
+##    any extra arguments will be handed through to solve().
+##
+##    If the supplied matrix is singular and error will be produced by the appropriate library (LAPACK by default).
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getInverse()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setInverse(m)
+	m
 }
